245225eddf1c0d42417bb1c8210e4ac2f5a2eb74
Puffbirbb <eaagmhc@students.eaaa.dk>
2022-02-23 14:11:12 +0100

    ?

--- L06-IfElse/src/Ex3ChildApp.java
+++ L06-IfElse/src/Ex3ChildApp.java
@@ -1,8 +1,8 @@
 public class Ex3ChildApp {
     public static void main(String[] args) {
         Ex3ChildClass child1 = new Ex3ChildClass(12, false);
-        child1.isMale(false);
-        child1.setAge(16);
+        child1.isMale(true);
+        child1.setAge(178);
         child1.printEx3ChildClass();
         child1.institution();
         child1.gender();
